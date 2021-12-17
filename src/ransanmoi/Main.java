832e1f5f08f109db7fd189a4c1c584cc83bca6c7
ErikHorus1249 <tuananh1421999@gmail.com>
2021-12-17 16:34:56 +0700

    first time

---
+++ src/ransanmoi/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ransanmoi;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author BENH VIEN CONG NGHE
+ */
+
+ class panel extends JPanel{
+     protected void paintComponent(Graphics g){
+         super.paintComponent(g);
+         ImageIcon img=new ImageIcon();
+     }
+ }
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
